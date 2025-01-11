73e082a442b652456f29e284ef87f51fa3c61431
Makr <116593190+Makr-Xie@users.noreply.github.com>
2025-01-11 11:42:03 -0600

    2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,29 @@ import './index.css'
 
 const Footer = () => {
     return (
-        <div className='footer-container'>Footer</div>
+        <div className='footer-container'>
+            <div className='footer-links'>
+                <a 
+                    href='https://github.com/Makr-Xie' 
+                    target='_blank' 
+                    rel='noopener noreferrer'
+                    className='footer-link'
+                >
+                    <i className='fab fa-github'></i> GitHub
+                </a>
+                <a 
+                    href='https://www.zhihu.com/people/xyx-98-63' 
+                    target='_blank' 
+                    rel='noopener noreferrer'
+                    className='footer-link'
+                >
+                    <i className='fab fa-zhihu'></i> Zhihu
+                </a>
+                <span className='footer-link'>
+                    <i className='fab fa-instagram'></i> Instagram
+                </span>
+            </div>
+        </div>
     )
 }
 
