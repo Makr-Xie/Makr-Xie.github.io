448c5f95b2e7cdfa19ad3d85adea9b4b7131cc15
Makr <116593190+Makr-Xie@users.noreply.github.com>
2025-01-11 11:44:31 -0600

    2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Makr-Xie.github.io/',
+  base: '/',  // Changed from '/Makr-Xie.github.io/' to '/' since it's a user/org site
   plugins: [react()],
 })
